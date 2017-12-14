a73fef3c7b93318777f96ae47718672979591d43
Nick Ginther <nginth@gmail.com>
2017-12-14 17:39:34 -0600

    add testing utility methods

--- app/tests.py
+++ app/tests.py
@@ -1,12 +1,38 @@
 import unittest
 import json
 from flask_testing import TestCase
-from app.app import create_app
+from .app import create_app, db
+from .models import Question
 
 
 class TestQuestionAPI(TestCase):
     def create_app(self):
         return create_app('test_config.json')
+
+    def setUp(self):
+        db.drop_all()
+        db.create_all()
+        self.populate_db()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def populate_db(self):
+        q1 = self.create_question(
+            "trees are fun", "balance this tree", "recursion")
+        q2 = self.create_question(
+            "trees are not fun", "red black tree", "hard work")
+
+        db.session.add_all((q1, q2))
+        db.session.commit()
+
+    def create_question(self, name, content, answer=None):
+        q = Question()
+        q.name = name
+        q.content = content
+        q.answer = answer
+        return q
 
     def test_fail(self):
         assert False
