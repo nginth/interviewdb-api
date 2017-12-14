7d82716f44bab25405efccdc38ad6382ee095e42
Nick Ginther <nginth@gmail.com>
2017-12-14 17:01:27 -0600

    add question creation endpoint

--- app/api/questions.py
+++ app/api/questions.py
@@ -1,8 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.models import Question
+from app.app import db
 
 questions_blueprint = Blueprint('questions', __name__)
 
 
-@questions_blueprint.route('/')
+@questions_blueprint.route('', methods=['GET'])
 def get_questions():
+    if request.method == 'POST':
+        return post_question()
+
     return "Hello"
+
+
+@questions_blueprint.route('', methods=['POST'])
+def post_question():
+    try:
+        new_question = question_from_json(request.get_json())
+        db.session.add(new_question)
+        db.session.commit()
+        response = jsonify({'message': 'Created.'})
+        response.status_code = 201
+        return response
+    except KeyError as err:
+        print(err.args)
+        response = jsonify(
+            {'message': 'Bad request. Request must contain field: ' + err.args[0] + '.', 'status_code': 400})
+        response.status_code = 400
+        return response
+
+
+def question_from_json(json):
+    question = Question()
+    question.name = json['name']
+    question.content = json['content']
+    question.answer = json['answer'] if 'answer' in json else None
+    return question
